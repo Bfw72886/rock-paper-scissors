443b48e96b95052bcb3023ca0ee36585c69e2d70
Bfw72886 <otto72886@m365.bfwbb-lernen.de>
2025-02-22 19:42:57 +0100

    Add playRound()

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNum = Math.random();
     if (randomNum <= 0.33) {
@@ -13,12 +16,36 @@ function getHumanChoice() {
     let isChoiceCorrect = false;
     while(!isChoiceCorrect) {
         let humanChoice = prompt("Choose rock, paper or scissors!")
-        if (humanChoice === "rock" | humanChoice === "paper" | humanChoice === "scissors") {
-            return humanChoice;
+        if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+            return humanChoice.toLowerCase();
         } else {
-            console.log("Please enter a valid choice. The choice is case-sensitive.");
+            alert("Please enter a valid choice.");
         }
     }
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    let capitalizedHumanChoice = String(humanChoice).charAt(0).toUpperCase() + String(humanChoice).slice(1);
+    let capitalizedComputerChoice = String(computerChoice).charAt(0).toUpperCase() + String(computerChoice).slice(1);
+    let humanWon = humanChoice == "rock" && computerChoice == "scissors" || 
+                    humanChoice == "paper" && computerChoice == "rock" ||
+                    humanChoice == "scissors" && computerChoice == "paper";
+    let computerWon = humanChoice == "scissors" && computerChoice == "rock" ||
+                    humanChoice == "rock" && computerChoice == "paper" ||
+                    humanChoice == "paper" && computerChoice == "scissors";
+    if (humanWon) {
+        humanScore++;
+        return "You won! " + capitalizedHumanChoice + " beats " + capitalizedComputerChoice;
+    } else if (computerWon) {
+        computerScore++;
+        return "You lost! " + capitalizedComputerChoice + " beats " + capitalizedHumanChoice;
+    } else {
+        return "It's a Draw. You both chose " + capitalizedHumanChoice;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+  
+console.log(playRound(humanSelection, computerSelection));
+  
