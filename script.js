561f1fa843d57626b15cad562f1f52991fe4145f
Bfw72886 <otto72886@m365.bfwbb-lernen.de>
2025-02-23 16:04:08 +0100

    Fix case-insensitivity of humanChoice

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function getComputerChoice() {
 function getHumanChoice() {
     let isChoiceCorrect = false;
     while(!isChoiceCorrect) {
-        let humanChoice = prompt("Choose rock, paper or scissors!")
-        if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
-            return humanChoice.toLowerCase();
+        let humanChoice = prompt("Choose rock, paper or scissors!").toLowerCase();
+        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+            return humanChoice;
         } else {
             alert("Please enter a valid choice.");
         }
@@ -27,12 +27,12 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     let capitalizedHumanChoice = String(humanChoice).charAt(0).toUpperCase() + String(humanChoice).slice(1);
     let capitalizedComputerChoice = String(computerChoice).charAt(0).toUpperCase() + String(computerChoice).slice(1);
-    let humanWon = humanChoice == "rock" && computerChoice == "scissors" || 
-                    humanChoice == "paper" && computerChoice == "rock" ||
-                    humanChoice == "scissors" && computerChoice == "paper";
-    let computerWon = humanChoice == "scissors" && computerChoice == "rock" ||
-                    humanChoice == "rock" && computerChoice == "paper" ||
-                    humanChoice == "paper" && computerChoice == "scissors";
+    let humanWon = humanChoice === "rock" && computerChoice === "scissors" || 
+                    humanChoice === "paper" && computerChoice === "rock" ||
+                    humanChoice === "scissors" && computerChoice === "paper";
+    let computerWon = humanChoice === "scissors" && computerChoice === "rock" ||
+                    humanChoice === "rock" && computerChoice === "paper" ||
+                    humanChoice === "paper" && computerChoice === "scissors";
     if (humanWon) {
         humanScore++;
         return "You won! " + capitalizedHumanChoice + " beats " + capitalizedComputerChoice;
@@ -44,8 +44,5 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-  
-console.log(playRound(humanSelection, computerSelection));
-  
+// TODO Step 6 & fix case
+console.log(playRound(getHumanChoice(), getComputerChoice()));
